e478fc859bf5647824b0aaf75b82616fbf9aa59f
Noa <noasalgadodev@gmail.com>
2023-03-05 18:55:26 +0100

    Feat: add header, homepage content and display content functionality

---
+++ src/Home/Home.js
@@ -0,0 +1,14 @@
+import './home-styles.css';
+
+export function home() {
+  const homeHtml = `
+    <section class="hero">
+        <div class="section-content">
+          <h1 class="main-heading">
+            Hawaiian food, <span>healthy lifestyle</span>
+          </h1>
+          <button class="btn">Order Now</button>
+        </div>
+    `;
+  return homeHtml;
+}
--- src/index.js
+++ src/index.js
@@ -1,1 +1,37 @@
+import { home } from './Home/Home';
+import './index.css';
 
+let currentPage = 'home';
+
+function addHandlers() {
+  const navBtns = document.querySelectorAll('.nav__btn');
+  navBtns.forEach((btn) =>
+    btn.addEventListener('click', (e) => {
+      currentPage = e.target.dataset.target;
+      navBtns.forEach((btn) => btn.classList.remove('active'));
+      e.target.classList.add('active');
+    })
+  );
+  console.log(navBtns);
+}
+
+function displayContent() {
+  const contentHtml = `
+  <header class="header">
+      <p class="logo">Poke Task</p>
+      <nav class="nav">
+        <button class="nav__btn active" data-target="home">Home</button>
+        <button class="nav__btn" data-target="menu">Menu</button>
+        <button class="nav__btn" data-target="contact">Contact</button>
+      </nav>
+    </header>
+    <main class="main">
+        ${home()}
+      </section>
+    </main>
+  `;
+  return contentHtml;
+}
+const contentEl = document.querySelector('#content');
+contentEl.innerHTML = displayContent();
+addHandlers();
